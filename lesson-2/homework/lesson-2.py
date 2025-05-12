3c66632de02618b5b5b3bc5cc433feafa7de2400
Timur Abdukarimov <67995666+Timaxrus@users.noreply.github.com>
2025-05-12 17:06:35 +0500

    Create lesson-2.py

---
+++ lesson-2/homework/lesson-2.py
@@ -0,0 +1,24 @@
+# 1.
+
+from datetime import datetime
+
+
+def calculate_age():
+    name = input("Enter your name").strip()          # Asks the user to enter their name and trims the accidental spaces before and after the name.
+    
+    year_of_birth = int(input("Enter your year of birth").strip())       # Asks the user to enter their year of birth and trims the accidental spaces before and after the entered year.
+    
+    current_year = datetime.now().year                    # Retrieves current year from the current date.
+    
+    age = current_year - year_of_birth          # Calcualtes the age.
+
+    return f"Hello {name}, your age is {age}."      # Output message "Hello [name of the user], your age is [year of birth of the user]."
+    
+print(calculate_age())
+
+
+# 2.
+
+
+                                
+  
