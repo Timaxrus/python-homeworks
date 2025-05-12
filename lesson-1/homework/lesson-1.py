227f6975338cb35f431f09ca3ac2c35990081328
Timur Abdukarimov <67995666+Timaxrus@users.noreply.github.com>
2025-05-12 13:40:24 +0500

    Update lesson-1.py

--- lesson-1/homework/lesson-1.py
+++ lesson-1/homework/lesson-1.py
@@ -1,1 +1,43 @@
+# 1.
 
+side = 10
+
+perimeter = side * 4
+
+area = side * side
+
+print("The area of the square: ", area, "\nThe perimeter of the square: ", perimeter)
+
+# 2.
+
+diameter = 10
+
+pi = 3.14159
+
+circle_length = diameter * pi
+
+print("The length of my circle is:", circle_length)
+
+# 3.
+
+a = 4
+
+b = 2
+
+print("The mean is:", (a + b) / 2)
+
+# 4.
+
+a = 4
+
+b = 2
+
+product = a * b
+
+sum = a + b
+
+squareA = a ** 2
+
+squareB = b ** 2
+
+print(f"The Product of a and b is {product} \nThe sum of a and b is {sum} \nThe square of a is {squareA} \nAnd the square of b is {squareB}")
